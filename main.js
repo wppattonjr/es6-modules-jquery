6e0166276676e11c962e409a256415ca662dd013
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-08-17 16:39:11 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+const bookInfo = [
+  {
+    price: 25.99,
+    title: "Fifty Shades of Chicken",
+    image: "chicken.jpg",
+    featured: false,
+  },
+  {
+    price: 15.45,
+    title: "Fifty Shades of Bacon",
+    image: "bacon.jpg",
+    featured: true,
+  },
+  {
+    price: 5,
+    title: "Fifty Shades of Kale",
+    image: "kale.jpg",
+    featured: false,
+  },
+  {
+    price: 2,
+    title: "Fifteen Shades for Grey",
+    image: "animals.jpeg",
+    featured: false,
+  },
+  {
+    price: 50,
+    title: "Fifty Shades of Zombie",
+    image: "zombie.jpg",
+    featured: true,
+  },
+  {
+    price: 0.75,
+    title: "Fifty Shades of Gravy",
+    image: "gravy.jpg",
+    featured: false,
+  },
+];
